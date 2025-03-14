ec2ee9706f2f48af8307f3229c13e3ab52e5388d
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-14 11:40:51 +0000

    Add 7.java

---
+++ src/7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class SchoolSciences {
+    public static void main(String[] args) {
+        double side1 = 2;
+        double side2 = 3;
+        double hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+        System.out.println("The length of the hypotenuse is " + hypotenuse);
+    }
+}
