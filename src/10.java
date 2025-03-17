a19aa9c6a90bfb57bc9f1b1a37c0c9f976ddd32a
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-17 14:18:13 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class SchoolSciences {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    System.out.println("Hello World");
+  }
+}
