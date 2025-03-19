06b05d74b8d11087b078030ded67ebbe930e1858
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-19 10:51:00 +0000

    Add 12.java

---
+++ src/12.java
@@ -0,0 +1,9 @@
+
+import java.util.Random;
+public class SchoolSciences {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(10);
+        System.out.println(number);
+    }
+}
