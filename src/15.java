50216964c80b34dfdef34275ea4dabf83c6a1f1d
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-22 16:25:06 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,11 @@
+public class ScientificCalc {
+    public static void main(String[] args) {
+        // Calculate simple arithmetic operations
+        double result1 = 2 + 3;
+        double result2 = 5 - 1.5;
+
+        // Print results
+        System.out.println("Result of addition: " + result1);
+        System.out.println("Result of subtraction: " + result2);
+    }
+}
