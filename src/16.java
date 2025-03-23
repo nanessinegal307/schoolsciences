d810b78486057b4c85f26b162abacfe5b538a9fd
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-23 04:26:29 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,7 @@
+public class ScientificCalculation {
+    public static void main(String[] args) {
+        double num1 = 2.0;
+        double num2 = 3.5;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
