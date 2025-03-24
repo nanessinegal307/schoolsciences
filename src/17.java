0118fd0ac04e9464b75e423c3c08d0aabb2c0468
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-24 13:50:45 +0000

    Add 17.java

---
+++ src/17.java
@@ -0,0 +1,7 @@
+public class ScienceProgram {
+    public static void main(String[] args) {
+        // Example program
+        System.out.println("Hello World!");
+        // Additional code here...
+    }
+}
