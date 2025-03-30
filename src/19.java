24ad1dbaef492e72e4a4f3240c36e2add27e51fd
nanessinegal307 <nanessinegal307@gmail.com>
2025-03-30 12:07:50 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,8 @@
+public class ScientificCalc {
+    public static void main(String[] args) {
+        double num1 = 3.5;
+        double num2 = 4.5;
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
