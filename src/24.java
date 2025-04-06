c145454ba81087d91194561093de7cba9c0dc9db
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-06 03:13:43 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,6 @@
+public class AdvancedCalculation {
+    public static void main(String[] args) {
+        double result = 10 + 2 * (3 - 5);
+        System.out.println("The result is: " + result);
+    }
+}
