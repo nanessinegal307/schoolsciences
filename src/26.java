617d0fbfcb78888edf321da162b43304492bd98c
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-08 18:41:21 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,9 @@
+public class MathCalculator {
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+}
