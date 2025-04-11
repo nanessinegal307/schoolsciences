425c36bab5aec5c88d5c3ee9cb57d02f8503c5a1
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 14:07:46 +0000

    Add 51.java

---
+++ src/51.java
@@ -0,0 +1,21 @@
+public class ScientificCalculation {
+    public static void main(String[] args) {
+        // Example: Addition
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Example: Subtraction
+        int difference = 8 - 4;
+        System.out.println("Difference between 8 and 4 is: " + difference);
+
+        // Example: Multiplication
+        int product = 9 * 3;
+        System.out.println("Product of 9 and 3 is: " + product);
+
+        // Example: Division
+        double quotient = 10.5 / 2;
+        System.out.println("Quotient of 10.5 divided by 2 is: " + quotient);
+    }
+}
