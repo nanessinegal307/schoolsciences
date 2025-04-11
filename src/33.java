8c07a56d03285f923d0a0c74d331ddeb9f7afd0f
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:33:26 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,9 @@
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        double num1 = 5.0;
+        double num2 = 3.0;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + (num1 - num2));
+    }
+}
