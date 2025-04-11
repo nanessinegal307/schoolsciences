caa7729740e9c51e859ab3c4d433700688f05593
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:52:41 +0000

    Add 43.java

---
+++ src/43.java
@@ -0,0 +1,11 @@
+public class ScientificCode {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 10.5;
+        String z = "Hello, World!";
+        
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+    }
+}
