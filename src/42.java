77253ad3dd7a2b2a2e57320a61380bbded7356f7
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:50:08 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,13 @@
+public class ScientificCodeGenerator {
+    public static void main(String[] args) {
+        // Example 1: Simple arithmetic operation
+        System.out.println("Result of 5 + 3 = " + (5 + 3));
+
+        // Example 2: Logical operators
+        System.out.println("Equal: a == b; Not equal: a != b");
+        System.out.println("Less than: a < b; Greater than: a > b");
+
+        // Example 3: Conditional statements
+        System.out.println("If true, do this: a = 10; Else if false, do that: a = 20;");
+    }
+}
