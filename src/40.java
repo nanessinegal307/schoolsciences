0d065ca675af659bbb674ef5e86c61b57ca2f842
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:46:43 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,12 @@
+public class ScienceCalculations {
+    public static void main(String[] args) {
+        // Example calculations
+        System.out.println("The area of a square with side 5 units is " + (5 * 5));
+        System.out.println("The volume of a cube with side 3 units is " + (3 * 3 * 3));
+
+        // More complex examples
+        double pi = Math.PI;
+        System.out.println("The circumference of a circle with radius 10 units is " + (2 * pi * 10));
+        System.out.println("The surface area of a sphere with radius 7 units is " + (4 * pi * Math.pow(7, 2)));
+    }
+}
