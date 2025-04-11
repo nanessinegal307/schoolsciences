f08c24cb045fd4726f0a472f1cbb316f32befca3
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:57:33 +0000

    Add 46.java

---
+++ src/46.java
@@ -0,0 +1,12 @@
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        double number1 = 5.0;
+        double number2 = 3.0;
+        
+        double sum = number1 + number2;
+        double product = number1 * number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+        System.out.println("The product of " + number1 + " and " + number2 + " is: " + product);
+    }
+}
