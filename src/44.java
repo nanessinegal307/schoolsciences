c94796bf1978df1d3a5803a5bfefbe33727b15fc
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:54:40 +0000

    Add 44.java

---
+++ src/44.java
@@ -0,0 +1,6 @@
+public class ScientificConcepts {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("I'm writing some code in my project schoolsciences.");
+    }
+}
