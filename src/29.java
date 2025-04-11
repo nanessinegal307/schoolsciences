de05a2394b373cc7d72b7bf8b66b11d040a20373
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:21:53 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,13 @@
+public class ScientificMath {
+    public static void main(String[] args) {
+        // Basic arithmetic operations
+        System.out.println("Addition: " + (5 + 3));
+        System.out.println("Subtraction: " + (7 - 2));
+        System.out.println("Multiplication: " + (6 * 4));
+        System.out.println("Division: " + (10 / 2));
+
+        // More complex operations
+        System.out.println("Exponentiation: " + (5L ** 3));
+        System.out.println("Modulus: " + (10 % 4));
+    }
+}
