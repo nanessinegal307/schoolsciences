dcacba1d590c3bb81384f57deaba096e6c441b8e
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 14:05:14 +0000

    Add 50.java

---
+++ src/50.java
@@ -0,0 +1,8 @@
+public class ScientificCalculator {
+    public static void main(String[] args) {
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
