50cbf3784d14151d5b903d324db4e50a63659012
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-11 12:48:10 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,9 @@
+public class ScientificCodeGenerator {
+    public static void main(String[] args) {
+        // Example: Simple formula calculation
+        double area = 3.14 * (radius^2);
+        
+        // Output result
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
