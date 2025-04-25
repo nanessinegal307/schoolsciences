d04b0a8b5e40524fd48409a20d7632e868233386
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-25 00:15:21 +0000

    Add 58.java

---
+++ src/58.java
@@ -0,0 +1,18 @@
+public class ScientificCalculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) throw new IllegalArgumentException("Division by zero is not allowed.");
+        return a / b;
+    }
+}
