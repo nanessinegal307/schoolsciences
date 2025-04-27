7d47841a343d3fde6576365442bd32c3a96a5552
nanessinegal307 <nanessinegal307@gmail.com>
2025-04-27 01:56:52 +0000

    Add 59.java

---
+++ src/59.java
@@ -0,0 +1,14 @@
+public class SciJava {
+    public static void main(String[] args) {
+        // Define variables
+        int x = 5;
+        double y = 3.14;
+        String z = "Hello World";
+
+        // Perform some operations
+        System.out.println("The result of the expression: x + y - z = " + (x + y - z));
+
+        // Print out a message
+        System.out.println("Here is your Java code example: \n");
+    }
+}
