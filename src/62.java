45937f57244d8847073e33e2e64b7fcbb8e4660f
nanessinegal307 <nanessinegal307@gmail.com>
2025-05-18 05:47:13 +0000

    Add 62.java

---
+++ src/62.java
@@ -0,0 +1,11 @@
+public class ScientificProgramming {
+    public static void main(String[] args) {
+        // Example Java code demonstrating some of the concepts and formulas in scientific disciplines
+        System.out.println("This is an example of a Java program.");
+        
+        // Example usage:
+        double radius = 5.0; // A common physical radius
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("The circumference of a circle with radius " + radius + " units is " + circumference);
+    }
+}
