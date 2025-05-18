221b671cb7dbbdc31ac16c23f55f57bd52160681
nanessinegal307 <nanessinegal307@gmail.com>
2025-05-18 02:38:54 +0000

    Add 61.java

---
+++ src/61.java
@@ -0,0 +1,7 @@
+public class ScientificCalculations {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("The area of the circle is: " + area);
+    }
+}
