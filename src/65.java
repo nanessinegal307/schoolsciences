cf000e187a586f1f38b97cd4b2988cecf78ca63a
nanessinegal307 <nanessinegal307@gmail.com>
2025-05-24 19:59:48 +0000

    Add 65.java

---
+++ src/65.java
@@ -0,0 +1,9 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        double num1 = 5.0;
+        double num2 = 3.0;
+
+        double result = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
