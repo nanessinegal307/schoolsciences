fc8208ac766dc269f625e7f78771faafa1faaf6b
nanessinegal307 <nanessinegal307@gmail.com>
2025-05-28 02:57:50 +0000

    Add 68.java

---
+++ src/68.java
@@ -0,0 +1,7 @@
+public class ScientificCalc {
+    public static void main(String[] args) {
+        // Example calculation: add 5 and 3, print the result
+        int sum = 5 + 3;
+        System.out.println("The result is: " + sum);
+    }
+}
